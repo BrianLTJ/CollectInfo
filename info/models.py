fba5e11b59738f968d3cc4385397e5ca0adad26a
Brian Lee <739142578@qq.com>
2016-09-16 11:06:15 +0800

    Add model

--- info/models.py
+++ info/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+	name = models.TextField(max_length=20)
+	phone = models.TextField(max_length=12)
+	qq = models.TextField(max_length=12)
+	dorm = models.TextField(max_length=50)
+	birthday = models.TextField(max_length=50)
+	place = models.TextField(max_length=100)
